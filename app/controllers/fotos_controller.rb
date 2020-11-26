d395dbce5534dd2eb69280ee3ad6d872d6b64944
matthiashaefeli <haefeli.matthias2gmail.com>
2020-11-26 14:06:14 -0700

    fotos upload

--- app/controllers/fotos_controller.rb
+++ app/controllers/fotos_controller.rb
@@ -2,4 +2,30 @@ class FotosController < ApplicationController
   def index
     @fotos = Foto.all
   end
+
+  def new
+    @foto = Foto.new
+  end
+
+  def create
+    @foto = Foto.new(foto_params)
+    if @foto.save
+      redirect_to fotos_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    foto = Foto.find(params[:id])
+    foto.main_image.purge
+    foto.delete
+    redirect_to fotos_path
+  end
+
+  private
+
+  def foto_params
+    params.require(:foto).permit(:title, :main_image)
+  end
 end
